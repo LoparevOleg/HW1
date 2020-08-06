8c94bdee56dd5c4ac918b7a5df205d323f31f9fa
LoparevOleg <olega199@mail.ru>
2020-08-06 15:30:04 +0500

    HW 6 Все задания выполнены, задание под * не додумал.

---
+++ src/ru/geekbrains/hw6/Cat.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.hw6;
+
+public class Cat extends Animal {
+    public Cat(String name, String color, int skillRun, int skillSwim) {
+        super(name, color, skillRun, skillSwim);
+    }
+}
